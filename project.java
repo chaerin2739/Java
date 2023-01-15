e22b35e2d23095e5a5e4f8765278d7bbc2cac30d
chaerin2739 <chlin2739@naver.com>
2023-01-15 18:54:03 +0900

    1/15 혼자

---
+++ project.java
@@ -0,0 +1,11 @@
+public class project {
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+    }
+}
+
+// 변수는 데이터를 담아두는 상자 약간 도구같은 느낌
+
+// int value; -> value 라는 상자 생성
+// int value = 9; -> value 라는 상자에 9를 집어넣음
+// c언어와 다르게 boolean이라는 논리형 변수가 있음 -> true false로 나타냄\
